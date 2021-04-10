7e1b19cb039813ec39c21db2023fd496890d7966
Mary Perret <mary.perret.1986@gmail.com>
2021-04-10 08:47:46 +0200

    add inline support

--- vite.config.inlined.ts
+++ vite.config.inlined.ts
@@ -1,18 +1,28 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { viteSingleFile } from "vite-plugin-singlefile"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    open: true,
-  },
+  plugins: [vue(), viteSingleFile()],
+	build: {
+		cssCodeSplit: false,
+		assetsInlineLimit: 100000000,
+		rollupOptions: {
+			output: {
+				manualChunks: () => "everything.js",
+			},
+		},
+	},
   resolve: {
     alias: [
       { find: '@', replacement: '/src' },
       { find: 'views', replacement: '/src/views' },
       { find: 'components', replacement: '/src/components' },
     ]
+  },
+  server: {
+    open: true,
   },
   define: {
     'process.env': process.env
