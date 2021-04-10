8ede54889db99d904be9080793b71e4a0788e9a2
Mary Perret <mary.perret.1986@gmail.com>
2021-04-10 07:34:54 +0200

    remove unecessary deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,5 @@
 import { defineConfig } from 'vite'
-import { getAliases } from 'vite-aliases'
 import vue from '@vitejs/plugin-vue'
-
-const aliases = getAliases();
 
 // https://vitejs.dev/config/
 export default defineConfig({
